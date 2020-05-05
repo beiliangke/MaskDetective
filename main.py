33d76306a97f4683de5867437e387bb66fad3c58
lunwei <1461545033@qq.com>
2020-05-05 23:57:55 +0800

    高斯模糊去噪点  加入音频再次失败

--- main.py
+++ main.py
@@ -30,8 +30,8 @@ def music_yes():
 def music_no():
     timeplay = 6
     while True:
-        pygame.mixer.music.load(yspeech)
-        print("-----戴口罩才是靓仔-----")
+        pygame.mixer.music.load(nspeech)
+        print("-----请您戴口罩-----")
         pygame.mixer.music.play()
         time.sleep(timeplay)
 
@@ -70,23 +70,26 @@ while True:
     if ret == False:
         print("无法打开摄像头")
         break
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #高斯模糊
+    blur=cv2.GaussianBlur(img, (5, 5), 0)
+    gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
     mean = np.mean(gray)
     gamma_val = math.log10(0.5) / math.log10(mean / 255)  # 公式计算gamma
     image_gamma_correct = gamma_trans(img, gamma_val)  # gamma变换
-    faces = detector.detectMultiScale(image_gamma_correct, 1.1, 3)
-    mask_face = mask_detector.detectMultiScale(image_gamma_correct, 1.1, 5)
+
+    faces = detector.detectMultiScale(image_gamma_correct, 1.1, 6)
+    mask_face = mask_detector.detectMultiScale(image_gamma_correct, 1.1, 4)
     # 参数分别为 图片、左上角坐标，右下角坐标，颜色，厚度
     # face = img[y:y + h, x:x + w]  # 裁剪坐标为[y0:y1, x0:x1]
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
         cv2.putText(img, 'NO MASK', (x - 25, y - 25), font, 1.2, (255, 0, 0), 2)
-        a = 0
+         # a = 0
     for (x2, y2, w2, h2) in mask_face:
         cv2.rectangle(img, (x2, y2), (x2 + w2, y2 + h2), (0, 0, 255), 2)
         cv2.putText(img, 'HAVE MASK', (x2 - 25, y2 - 25), font, 1.2, (0, 0, 255), 2)
-        a = 1
-    eyes = eyes_detector.detectMultiScale(image_gamma_correct, 1.1, 3)
+        # a = 1
+    eyes = eyes_detector.detectMultiScale(image_gamma_correct, 1.1, 5)
     for (x3, y3, w3, h3) in eyes:
         cv2.rectangle(img, (x3, y3), (x3 + w3, y3 + h3), (0, 255, 0), 2)
 
@@ -97,12 +100,16 @@ while True:
     # elif a == 1:
     #     print("戴口罩------------")
     #     threading.Thread(target=music_yes).start()
-
     cv2.imshow('mask', img)
     c = cv2.waitKey(30) & 0xff
     if c == 27:
         cap.release()
         break
+
+    # while mask_face == ():
+    #     print("------------无口罩")
+    #     threading.Thread(target=music_no).start()
+    #     time.sleep(1)
 
 cap.release()
 cv2.destroyAllWindows()
